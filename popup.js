399879e42e771b9ee6cd7e494311531a574fc1cd
Thydekar <130446887+Thydekar@users.noreply.github.com>
2023-11-06 03:00:08 -0800

    Add files via upload
    The Deal with this one is you have to create your own voice files that correspond to the words. just look at the code

--- popup.js
+++ popup.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var searchButton = document.getElementById('searchButton');
+  const speakButton = document.getElementById('speakButton');
+  speakButton.addEventListener('click', function() {
+    const textArea = document.getElementById('textArea');
+    const words = textArea.value.split(' ');
 
-  searchButton.addEventListener('click', function() {
-    var searchText = document.getElementById('searchText').value;
-    var proxyUrl = 'https://google.com';
-
-    
-    var finalUrl = proxyUrl + '/search?q=' + encodeURIComponent(searchText);
-    
-    chrome.tabs.create({ url: finalUrl });
+    words.forEach(function(word, index) {
+      const audioSrc = chrome.runtime.getURL(word.toLowerCase() + '.mp3');
+      const audio = new Audio(audioSrc);
+      setTimeout(function() {
+        audio.play();
+      }, index * 1000); // Delay each audio file by 1 second (1000 milliseconds)
+    });
   });
 });

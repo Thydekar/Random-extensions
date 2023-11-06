4c12a4977bcff6cc96702dafdad15075ad4d0779
Thydekar <130446887+Thydekar@users.noreply.github.com>
2023-11-06 02:50:53 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,12 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   searchButton.addEventListener('click', function() {
     var searchText = document.getElementById('searchText').value;
-    var proxyUrl = 'https://google.com'; // Replace with your proxy URL
+    var proxyUrl = 'https://google.com';
 
     
     var finalUrl = proxyUrl + '/search?q=' + encodeURIComponent(searchText);
-
-    // Open the final URL in a new tab
+    
     chrome.tabs.create({ url: finalUrl });
   });
 });
